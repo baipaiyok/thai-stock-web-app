9991d74f87f86e067ee23019156c088631e16bdb
baipaiyok <baipaiyok@gmail.com>
2026-03-04 22:36:19 +0700

    add backend url

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -8,7 +8,7 @@ function App() {
 
 const fetchData = async () => {
     try {
-      const res = await axios.get(`${import.meta.env.VITE_API_URL}/api/stocks`);
+      const res = await axios.get(`https://thai-stock-web-app.onrender.com/api/stocks`);
       setStocks(res.data);
       setLoading(false);
     } catch (err) {
